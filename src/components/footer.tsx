6370c1f0e9db16382eb91d61f822ddc4bb08bb8d
Ka Anwir <132270018+Ka-NoMoney@users.noreply.github.com>
2025-03-28 22:05:52 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="py-4 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} Nguyễn Thanh Tuấn. All rights reserved.
+      © {new Date().getFullYear()} Nguyễn Trọng Thảo. All rights reserved.
     </footer>
   );
 }
